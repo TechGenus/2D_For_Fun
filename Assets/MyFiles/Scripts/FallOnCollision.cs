0dcca83ee1c5bea51b5a622866b5935a9963da9b
Janakkumar Bhanushali <jbhanush@ws.cs.ryerson.ca>
2020-02-04 20:22:25 -0500

    First commit finished level 0

---
+++ Assets/MyFiles/Scripts/FallOnCollision.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallOnCollision : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D col)
+	{
+		transform.parent.gameObject.GetComponent<Rigidbody2D>().bodyType = RigidbodyType2D.Dynamic;
+	}
+}
